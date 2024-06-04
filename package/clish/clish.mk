cb81d9b0e04b607a56fcbdacfe918f9ef1b1e59e
Yingxiang Yao <yingxiang.yao@kasumi321.de>
2024-06-04 21:02:39 +0200

    Resolved build-error

--- package/clish/clish.mk
+++ package/clish/clish.mk
@@ -1,8 +1,10 @@
 CLISH_VERSION = 0.7.4
 CLISH_DEPENCENCIES = ncurses readline libxml2
 CLISH_SITE = https://github.com/wyx227/clish/releases/download
-CLISH_SOURCE = https://github.com/wyx227/clish/releases/download/0.7.4/clish-0.7.4.tar.gz
-CLISH_INSTALL_STAGING = NO
+#CLISH_SOURCE = 0.7.4/clish-0.7.4.tar.gz
+CLISH_SOURCE = $(CLISH_VERSION)/clish-$(CLISH_VERSION).tar.gz
+CLISH_INSTALL_STAGING = YES
 CLISH_INSTALL_TARGET = YES
+CLISH_MAKE_OPTS += -L $(BUILD_DIR)/clish-$(CLISH_VERSION)
 
 $(eval $(autotools-package))
