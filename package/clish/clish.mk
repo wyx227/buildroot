8fe01751c5a0d07e125f3cc3d2a13d8df3ca94a1
Yingxiang Yao <yingxiang.yao@kasumi321.de>
2024-06-07 00:34:38 +0200

    Install clish config to the default config folder

--- package/clish/clish.mk
+++ package/clish/clish.mk
@@ -5,6 +5,9 @@ CLISH_SITE = https://github.com/wyx227/clish/releases/download
 CLISH_SOURCE = $(CLISH_VERSION)/clish-$(CLISH_VERSION).tar.gz
 CLISH_INSTALL_STAGING = YES
 CLISH_INSTALL_TARGET = YES
-CLISH_MAKE_OPTS += -L $(BUILD_DIR)/clish-$(CLISH_VERSION)
+CLISH_MAKE_OPTS += -L$(BUILD_DIR)/clish-0.7.4/
+
+mkdir -p $(TARGET_DIR)/etc/clish/
+cp -r $(BUILD_DIR)/xml-examples/* $(TARGET_DIR)/etc/clish/
 
 $(eval $(autotools-package))
